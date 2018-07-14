692137afaba544a40fc8fd42fede5c4ee2cd2648
Wang.Jian <wang.jian@YCH.COM>
2018-07-14 09:18:36 +0800

    create

---
+++ provider-report-service/script/shell/startup.bat
@@ -0,0 +1,4 @@
+title "### Megrez.provider_report_service 2.x ###"
+set JAR_NAME=Megrez.provider_report_service.jar
+set JAR_FILEPATH=C:\work\Megrez\2.x\Megrez_provider_common
+java -jar -Xms512m -Xmx1024m -Dspring.config.location=../config/application.yml ../jar/%JAR_NAME%
